a32c5dffa4a1ce01e3dc57238e768fcd7d7753bd
Danielwhyte <danwhy@gmail.com>
2018-09-27 12:49:14 +0100

    mix new

---
+++ lib/autoform.ex
@@ -0,0 +1,18 @@
+defmodule Autoform do
+  @moduledoc """
+  Documentation for Autoform.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Autoform.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/autoform_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoformTest do
+  use ExUnit.Case
+  doctest Autoform
+
+  test "greets the world" do
+    assert Autoform.hello() == :world
+  end
+end
