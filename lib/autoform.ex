89cb8efcb27227509744636ec7260f5ce34867e8
Danielwhyte <danwhy@gmail.com>
2018-10-09 11:43:07 +0100

    displays existing associations

--- lib/autoform.ex
+++ lib/autoform.ex
@@ -17,6 +17,8 @@ defmodule Autoform do
   """
   defmacro __using__(_opts) do
     quote location: :keep do
+      import Ecto.Query, only: [from: 2]
+
       @doc """
         Renders a 'new' or 'update' form based on the schema passed to it.
 
@@ -34,6 +36,8 @@ defmodule Autoform do
 
           * `:assigns` - The assigns for the form template, for example a changeset for update forms. Will default to empty list
           * `:exclude` - A list of any fields in your schema you don't want to display on the form
+          * `:update_field` - The field from your schema you want to use in your update path (/users/some-id), defaults to id
+          * `:assoc_query` - An ecto query you want to use when loading your associations
 
       """
       @spec render_autoform(
@@ -46,15 +50,26 @@ defmodule Autoform do
           when action in [:update, :create] do
         assigns = Keyword.get(options, :assigns, [])
 
-        {_key, schema_data} =
+        path_data =
           case action do
-            :create -> {nil, []}
-            :update -> Enum.find(assigns, fn {_k, v} -> match?(v, schema) end)
+            :create ->
+              []
+
+            :update ->
+              {key, schema_data} =
+                Enum.find(assigns, fn {k, v} ->
+                  k != :changeset && match?(v, schema)
+                end)
+
+              case Keyword.get(options, :update_field) do
+                nil -> schema_data
+                field -> Map.get(schema_data, field)
+              end
           end
 
         required = Map.get(schema.changeset(struct(schema), %{}), :required)
 
-        action = path(conn, action, schema_data)
+        action = path(conn, action, path_data)
 
         assigns =
           assigns
@@ -107,6 +122,11 @@ defmodule Autoform do
       defp put_associations(assigns, conn, schema, options) do
         excludes = Keyword.get(options, :exclude, [])
 
+        assoc_query =
+          Keyword.get(options, :assoc_query, fn schema -> from(s in schema, select: s) end)
+
+        ch = Map.get(assigns, :changeset).data
+
         repo =
           Module.concat(
             Macro.camelize(to_string(Phoenix.Controller.endpoint_module(conn).config(:otp_app))),
@@ -119,12 +139,22 @@ defmodule Autoform do
           |> Enum.map(fn a ->
             assoc = Map.get(schema.__schema__(:association, a), :queryable)
 
+            query =
+              case is_function(assoc_query) do
+                true -> assoc_query.(assoc)
+                false -> nil
+              end
+
             %{
               name: a,
               associations:
-                Enum.map(apply(repo, :all, [assoc]), fn a ->
-                  Map.put(a, :display, Map.get(a, :name, Map.get(a, :type, "test")))
-                end)
+                Enum.map(
+                  apply(repo, :all, [query]),
+                  fn a ->
+                    Map.put(a, :display, Map.get(a, :name))
+                  end
+                ),
+              loaded_associations: apply(repo, :preload, [ch, [{a, query}]])
             }
           end)
 
