64443d8642d1f2043b35ae232f688b22936c70d8
Danielwhyte <danwhy@gmail.com>
2018-10-22 10:07:09 +0100

    applies custom labels to associations, #18

--- lib/views/custom_view.ex
+++ lib/views/custom_view.ex
@@ -8,4 +8,12 @@ defmodule Autoform.CustomView do
     type = Phoenix.HTML.Form.input_type(form, field)
     apply(Phoenix.HTML.Form, type, [String.to_existing_atom(name), field, opts])
   end
+
+  def get_label(element, field) do
+    class = "control-label #{if field in element.required, do: "required"}"
+
+    label String.to_existing_atom(element.schema_name), field, class: class do
+      Map.get(element.custom_labels, field, humanize(field))
+    end
+  end
 end
